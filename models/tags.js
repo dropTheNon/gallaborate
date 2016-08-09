2b1d431fbd7a6824b2b8867f7edd3abdddd42877
Josh McBroom <mcbroomjc@gmail.com>
2016-08-09 14:03:29 -0700

    Added tag model

---
+++ models/tags.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var tagSchema = new mongoose.Schema({
+  name: { type: String, required: true }
+});
+
+var Tag = mongoose.model('Tag', tagSchema);
+
+module.exports = Tag;
